06d03bb820bc016b052d20318663422d07578c37
Baxtiyorjon <108717532+BaxtiyorjonAbduqahhorov@users.noreply.github.com>
2022-10-14 16:55:14 +0500

    black appbar

--- lib/extra/custom_black_appbar.dart
+++ lib/extra/custom_black_appbar.dart
@@ -1,7 +1,10 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hansa_lab/blocs/menu_events_bloc.dart';
 import 'package:hansa_lab/extra/hamburger.dart';
+import 'package:hansa_lab/page_routes/bottom_slide_page_route.dart';
+import 'package:hansa_lab/screens/search_screen.dart';
 import 'package:provider/provider.dart';
 
 class CustomBlackAppBar extends StatelessWidget {
@@ -11,7 +14,8 @@ class CustomBlackAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final providerScaffoldKey = Provider.of<GlobalKey<ScaffoldState>>(context);
     final providerChewieController = Provider.of<ChewieController>(context);
-
+    final menuProvider = Provider.of<MenuEventsBloC>(context);
+    final token = Provider.of<String>(context);
     return Container(
       height: 81.h,
       color: const Color(0xff333333),
@@ -33,17 +37,40 @@ class CustomBlackAppBar extends StatelessWidget {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/hansa_white.png',
-                height: 25.h,
-                width: 214.w,
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                  providerChewieController
+                    ..seekTo(Duration.zero)
+                    ..pause();
+                  menuProvider.eventSink.add(MenuActions.welcome);
+                },
+                child: Image.asset(
+                  'assets/hansa_white.png',
+                  height: 25.h,
+                  width: 214.w,
+                ),
               ),
             ],
           ),
-          Icon(
-            Icons.search,
-            size: 25.sp,
-            color: const Color(0xffffffff),
+          InkWell(
+            onTap: () {
+              Navigator.pop(context);
+              providerChewieController
+                ..seekTo(Duration.zero)
+                ..pause();
+              Navigator.of(context).push(SlideTransitionBottom(
+                Provider.value(
+                  value: token,
+                  child: const SearchScreen(),
+                ),
+              ));
+            },
+            child: Icon(
+              Icons.search,
+              size: 25.sp,
+              color: const Color(0xffffffff),
+            ),
           ),
         ],
       ),
